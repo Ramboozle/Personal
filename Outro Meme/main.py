d5a71ea2cdd05c84a9ac6dec76deeed445e51061
OliverNield <206021@gardenschool.edu.my>
2022-09-10 20:42:11 +0800

    Fixed the SU problems
    Fixed the problems with the super user now the code works as it should! I have also put the original inspiration for the project in the file too

--- Outro Meme/main.py
+++ Outro Meme/main.py
@@ -2,7 +2,6 @@ import vlc,time,os
 outro = vlc.MediaPlayer("TheFatRat-Xenogenesis.mp3")
 outro.play()
 outro.set_time(47000)
-time.sleep(9.5)
-os.system("shutdown -h now") #will not work and needs SU :(
-time.sleep(5) #if I could get it to run in SU I would time it better 
+time.sleep(11)
+os.system("shutdown -h now")
 outro.stop()
