1d75410cc62f0cd2c2fbced1db0bc61abdb141fe
OliverNield <206021@gardenschool.edu.my>
2022-09-14 11:30:57 +0800

    Changed outro meme a lil bit

--- Outro Meme/main.py
+++ Outro Meme/main.py
@@ -2,6 +2,6 @@ import vlc,time,os
 outro = vlc.MediaPlayer("TheFatRat-Xenogenesis.mp3")
 outro.play()
 outro.set_time(47000)
-time.sleep(11)
+time.sleep(10.2)
 os.system("shutdown -h now")
 outro.stop()
