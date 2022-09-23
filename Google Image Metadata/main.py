5cf310b9445b57e5311ec6c361b2ec1101e01354
OliverNield <206021@gardenschool.edu.my>
2022-09-23 22:37:04 +0800

    google photos project removed
    project was useless becuase images from google still have metadata so my whole idea is not needed

--- Google Image Metadata/main.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image, ExifTags
-img = Image.open("/Users/ramboozle/Library/Mobile Documents/com~apple~CloudDocs/Python/Personal/Google Image Metadata/Tester Photos/2.jpg")
-exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
-print(exif)
