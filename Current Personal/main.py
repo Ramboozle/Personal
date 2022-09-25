87f27fb13a666d8af151aa5663fab99d7b7ac4e4
OliverNield <206021@gardenschool.edu.my>
2022-09-25 18:11:11 +0800

    Started learning swift :)

--- Current Personal/main.py
+++ Current Personal/main.py
@@ -1,1 +1,14 @@
-print('poopie stinkie heeheehoo')
+def quicksort(arr):
+    length = len(arr)
+    if length <= 1:
+        return arr
+    pivot = arr.pop()
+    greater = []
+    less = []
+    for number in arr:
+        if number > pivot:
+            greater.append(number)
+        else:
+            less.append(number)
+    return(quicksort(less)+[pivot]+quicksort((greater)))
+print(quicksort([1,5,7,8,4,3,6,8,9,5,4,2,6,8,9]))
